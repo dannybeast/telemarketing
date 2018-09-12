6f9b80b0934c844340a138e1701b25a333b07c3a
Danil <dannybeastrus@gmail.com>
2018-09-12 15:21:38 +0300

    первый старт

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+  // loader
+	$(".loader").fadeOut("slow");
+
+  console.log('penis');
+
+})
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzY3JpcHRzLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIiQoZG9jdW1lbnQpLnJlYWR5KGZ1bmN0aW9uKCl7XHJcblxyXG4gIC8vIGxvYWRlclxyXG5cdCQoXCIubG9hZGVyXCIpLmZhZGVPdXQoXCJzbG93XCIpO1xyXG5cclxuICBjb25zb2xlLmxvZygnaGdoJyk7XHJcbiAgXHJcbn0pXHJcbiJdLCJmaWxlIjoic2NyaXB0cy5qcyJ9
