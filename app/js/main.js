edfc7577bf974728361455ebe3ff71a2aed157de
danil <d.barsukov@danycom.ru>
2018-09-19 11:16:35 +0300

    добавил подменю на десктопах

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,33 @@ $(document).ready(function(){
   // loader
   $(".loader").fadeOut("slow");
   
+  // podmenu
+
+  $('.header__item').click(function(){
+    if ( $(this).children('.header__link').hasClass('active') ){
+      $('.header-podmenu').removeClass('show');
+      $('.header__link').removeClass('active');
+    }else{
+      $('.header-podmenu').removeClass('show');
+      $('.header__link').removeClass('active');
+      $(this).children('.header__link').toggleClass('active');
+      $(this).children('.header-podmenu').toggleClass('show');
+    }
+
+
+  });
+  
+  $(document).mouseup(function (e){ 
+    var div = $(".header__item, .header-podmenu");
+    if (!div.is(e.target) 
+        && div.has(e.target).length === 0) { 
+      $('.header-podmenu').removeClass('show');
+      $('.header__link').removeClass('active');
+    }
+});
+
+
+
   // mobile menu
   $('.nav-icon-wrap').click(function(){
     $(this).toggleClass('open');
@@ -13,8 +40,8 @@ $(document).ready(function(){
   });
 
   // tabs
-
   $('.tabs').tabs();
+
 
 })
 
