545bbe3e01d14124e62c1163ed5343128f6bf193
Nick <d-key266@yandex.ru>
2018-09-20 10:46:53 +0300

    Часть страницы Telemarketing-Online

--- build/js/main.js
+++ build/js/main.js
@@ -3,6 +3,27 @@ $(document).ready(function(){
   // loader
   $(".loader").fadeOut("slow");
   
+  // podmenu
+  $('.header__item').click(function(){
+    if ( $(this).children('.header__link').hasClass('active') ){
+      $('.header-podmenu').removeClass('show');
+      $('.header__link').removeClass('active');
+    }else{
+      $('.header-podmenu').removeClass('show');
+      $('.header__link').removeClass('active');
+      $(this).children('.header__link').toggleClass('active');
+      $(this).children('.header-podmenu').toggleClass('show');
+    }
+  });
+  $(document).mouseup(function (e){ 
+    var div = $(".header__item, .header-podmenu");
+    if (!div.is(e.target) 
+        && div.has(e.target).length === 0) { 
+      $('.header-podmenu').removeClass('show');
+      $('.header__link').removeClass('active');
+    }
+  });
+
   // mobile menu
   $('.nav-icon-wrap').click(function(){
     $(this).toggleClass('open');
@@ -13,8 +34,37 @@ $(document).ready(function(){
   });
 
   // tabs
-
   $('.tabs').tabs();
+
+
+  // input
+  $(".input-block input").focus(function(){
+    $(this).parent().addClass("input-block--focus");
+     }).blur(function(){
+      $(this).parent().removeClass("input-block--focus");
+     });
+     // проверка на ввод в инпут
+     $('.input-block input').blur(function(){
+    if($(this).val() == ''){
+      $(this).parent().removeClass("input-block--focus");
+    }else{
+      $(this).parent().addClass("input-block--focus");
+    }
+     });
+
+  // show-hide form
+  $('.js-btn-show').click(function(){
+
+    $('.form-section .form-block').slideToggle();
+    var btn_text = $('.js-btn-show').text();
+    $(this).text(
+      btn_text == "Закрыть форму" ? "Начать" : "Закрыть форму");
+
+  });
+
+  // input masks
+  $('.js-phone-mask').inputmask("mask", {"mask": "+7 (999) 999-99-99", 'placeholder':'+7 (___) ___-__-__'});
+
 
 })
 
